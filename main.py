320099c947c50e844e9051b5568e27f0f979fbf5
im612 <imereu.cnio@gmail.com>
2023-05-10 00:02:52 +0200

    commit v174 - s3 be - indnames fe

--- main.py
+++ main.py
@@ -35,30 +35,25 @@ access_id = st.secrets['AWS_ACCESS_KEY_ID']
 access_key = st.secrets['AWS_SECRET_ACCESS_KEY']
 aws_bucket = 'p7-bucket'
 #
-# @st.cache_data(ttl=3600)
-# def get_df():
-#     global df # https://www.w3schools.com/python/python_variables_global.asp
-#     df = pd.read_csv(f"s3://{aws_bucket}/test_split_orig.csv",
-#                      storage_options={'key': access_id, 'secret': access_key})
-#     # https: // s3fs.readthedocs.io / en / latest / api.html # s3fs.core.S3FileSystem
-#     return df
-#
-# df = get_df()
-# st.write(df.shape)
+@st.cache_data(ttl=3600)
+def get_df():
+    global df # https://www.w3schools.com/python/python_variables_global.asp
+    df = pd.read_csv(f"s3://{aws_bucket}/test_split_orig.csv",
+                     storage_options={'key': access_id, 'secret': access_key})
+    # https: // s3fs.readthedocs.io / en / latest / api.html # s3fs.core.S3FileSystem
+    return df
 
-# df = get_df()
-# st.write('ok df')
+df = get_df()
+st.write(df.shape)
 
-#
-# #
-# #
-# @st.cache_data(ttl=3600)
-# def load_indnames():
-#     indnames = pd.DataFrame(df, columns=['SK_ID_CURR']).astype(int).values
-#     del df
-#     merged = list(chain.from_iterable(indnames.tolist()))
-#     return merged
+@st.cache_data(ttl=3600)
+def load_indnames():
+    indnames = pd.DataFrame(df, columns=['SK_ID_CURR']).astype(int).values
+    # del df
+    merged = list(chain.from_iterable(indnames.tolist()))
+    return merged
 
+indnames = load_indnames()
 #
 # # # importation des indnames
 # # # https://docs.streamlit.io/library/advanced-features/caching#controlling-cache-size-and-duration
@@ -80,16 +75,16 @@ aws_bucket = 'p7-bucket'
 id = st.selectbox("Saisir le code client :", [i for i in indnames])
 st.header(f'Code client: {str(int(id))}')
 #
-@st.cache_data(ttl=3600)  # 👈 Add the caching decorator
-def load_indnames2():
-    response = requests.post(url=f"{urlname}/indnames")
-#     # indnames = requests.post(url=f"{urlname2}/indnames")
-#     response = load_indnames()
-    objind = response.json()
-    indnames = objind['listindnames']
-    return indnames
-
-indnames = load_indnames2()
+# @st.cache_data(ttl=3600)  # 👈 Add the caching decorator
+# def load_indnames2():
+#     response = requests.post(url=f"{urlname}/indnames")
+# #     # indnames = requests.post(url=f"{urlname2}/indnames")
+# #     response = load_indnames()
+#     objind = response.json()
+#     indnames = objind['listindnames']
+#     return indnames
+#
+# indnames = load_indnames2()
 
 # # # SELECTION NUMERO CLIENT
 id = st.selectbox("Saisir le code client :", [i for i in indnames])
