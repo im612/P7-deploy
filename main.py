3cd84be7d0929191f387f1fadf15c6b2f1a151ad
im612 <imereu.cnio@gmail.com>
2023-05-16 22:17:55 +0200

    commit

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ del colnames_100[-1]
 # 4. Valeurs SHAP
 shap_values = pd.DataFrame(explainer.shap_values(x_line)[0], index=colnames, columns=['shap']) #orizzontale?
 shap_sorted = shap_values.sort_values(by=['shap'])
-shap_values_highest = shap_sorted.tail(10)
+shap_values_highest = shap_sorted.tail(10)[::-1]
 st.write('shap_values_highest')
 st.write(shap_values_highest)
 shap_values_lowest = shap_sorted.head(10)
@@ -183,7 +183,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # fig, ax = plt.subplots(figsize=(0.6, 1.5))
-fig, ax = plt.subplots(figsize=(1,1.6))
+fig, ax = plt.subplots(figsize=(1.2,1.6))
 # ax= sns.lineplot(data=df2, markers= True)
 # ax = sns.barplot(x=shap_values_highest["shap"].index, y=shap_values_highest["shap"], orient='h', color="g")
 ax = sns.barplot(x=shap_values_highest["shap"], y=shap_values_highest["shap"].index, orient='h', color="g")
@@ -191,10 +191,10 @@ ax = sns.barplot(x=shap_values_highest["shap"], y=shap_values_highest["shap"].in
 # ax.set(xlim=(0, shap_values_highest["shap"].max()*1.1), xlabel='Facteurs', ylabel='Valeurs SHAP', title='Facteurs favorables')
 ax.set(xlim=(0, shap_values_highest["shap"].max()*1.1))
 # ax.set(xlabel='Facteurs', fontsize=15)
-plt.xlabel('Facteurs', fontsize=12)
-plt.xticks(fontsize=12)
+plt.xlabel('Facteurs', fontsize=8)
+plt.xticks(fontsize=9)
 # ax.set(ylabel='Valeurs SHAP', fontsize=15)
-plt.ylabel('Valeurs SHAP', fontsize=12)
+plt.ylabel('Valeurs SHAP', fontsize=8)
 # ax.xaxis.set_tick_params(labelsize='small', fontsize=12)
 # https://stackoverflow.com/questions/12444716/how-do-i-set-the-figure-title-and-axes-labels-font-size
 # ax.set(xlim=(0, 24), xlabel='Facteurs', ylabel='ylabel', title='Facteurs avec un impact positif')
